ec3974ba707ffe8f17a6f176dfbbd641837ac52e
andysharky <heckermanacc@gmail.com>
2023-09-27 13:32:37 +0300

    Update Program.cs

--- Andrenium/Program.cs
+++ Andrenium/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            string JErryName = Console.ReadLine();
+            if (JErryName == "Jery") 
+            {
+                Console.WriteLine(JErryName);
+            }
+
+            
+
         }
     }
 }
