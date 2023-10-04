48ed4f08f7d0c5c74e4b0d5c589ddb83c1cda963
andysharky <heckermanacc@gmail.com>
2023-10-04 11:11:29 +0300

    f

---
+++ School/DataTpes And Varibles HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace JeriMoment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            switch(input)
+            {
+                case 1:
+                    int first = int.Parse(Console.ReadLine());
+
+
+                    int second = int.Parse(Console.ReadLine());
+
+
+                    int third = int.Parse(Console.ReadLine());
+
+
+                    int fourth = int.Parse(Console.ReadLine());
+
+
+                    double sumResult = first + second;
+                    double divisionResult = (sumResult / third);
+                    double multiplicationResult = divisionResult * fourth;
+
+
+                    Console.WriteLine(multiplicationResult);
+                    break;
+
+                case 2:
+                    int Number = int.Parse(Console.ReadLine());
+                    double NumSum = 0;
+                    while(Number > 0)
+                    {
+                        Number = Number / 10;
+                        
+                        NumSum += Number % 10;
+                    }
+                    Console.WriteLine(NumSum);
+                    break;
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+
+                case 11:
+
+                    break;
+                    
+            }
+            
+        }
+    }
+}
