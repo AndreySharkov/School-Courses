e64a4b813623d535021136aacfb1471098004873
andysharky <heckermanacc@gmail.com>
2023-11-07 12:06:56 +0200

    Methods

---
+++ School/Methods Exercises/CharsInRange/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CharsInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            PrintChars(firstChar, secondChar);
+        
+            static void PrintChars(char char1, char char2)
+            {
+                int charInNum1 = char1;
+                int charInNum2 = char2;
+                for (int i = char1; i < charInNum2; i++)
+                {
+                    Console.Write(i++ + " ");
+                }
+            }
+    }
+}
