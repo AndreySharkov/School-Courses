58c9e1e00f6f13b4486c744e9c8e81a71670d010
andysharky <heckermanacc@gmail.com>
2023-11-07 12:21:54 +0200

    Methods

--- School/Methods Exercises/CharsInRange/Program.cs
+++ School/Methods Exercises/CharsInRange/Program.cs
@@ -7,15 +7,30 @@
             char firstChar = char.Parse(Console.ReadLine());
             char secondChar = char.Parse(Console.ReadLine());
             PrintChars(firstChar, secondChar);
-        
+
             static void PrintChars(char char1, char char2)
             {
                 int charInNum1 = char1;
                 int charInNum2 = char2;
-                for (int i = char1; i < charInNum2; i++)
+                if (charInNum1 < charInNum2)
                 {
-                    Console.Write(i++ + " ");
+                    for (int i = ++charInNum1; i < charInNum2; i++)
+                    {
+
+                        Console.Write(Convert.ToChar(i) + " ");
+                    }
                 }
+                else
+                {
+                    for (int i = ++charInNum2; i < charInNum1; i++)
+                    {
+
+                        Console.Write(Convert.ToChar(i) + " ");
+                    }
+                }
+                
+                Console.WriteLine();
             }
+        }
     }
 }
