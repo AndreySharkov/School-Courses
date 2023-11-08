32391fe1df83629cbb67329d14d6adef9f78ef51
andysharky <heckermanacc@gmail.com>
2023-11-08 08:19:04 +0200

    Password Validator

--- School/Methods Exercises/PasswordValidator/Program.cs
+++ School/Methods Exercises/PasswordValidator/Program.cs
@@ -9,33 +9,51 @@
         }
         static void ValidatePassword(string password)
         {
-            string notAllowedSymboolls = "!@#$%^&**()_+-=[{}|]:;?.<>$";
-            string Nums = "1234567890";
-            bool badPass = false;
-            if (password.Length >= 6 &&  password.Length <= 10) { }
-            else
+            int check = 0;
+            if (HasRequiredPassWordSize(password)) check++;
+
+            if(HasEnoughtDigitAccept(password))check++;
+
+            if(HasRequiredAlphanumeriCharacters(password)) check++;
+
+            if (check == 3)
             {
-                badPass = true;
-                Console.WriteLine("Password must be between 6 and 10 characters");
+                Console.WriteLine("Password is valid");
             }
 
-            int AcceptPass = 0;
+        }
+        static bool HasRequiredPassWordSize(string password)
+        {
+            
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            else
+            {
+                
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                return false;
+            }
+        }
+        static bool HasRequiredAlphanumeriCharacters(string password)
+        {
+            string notAllowedSymboolls = "!@#$%^&**()_+-=[{}|]:;?.<>$";
             foreach (char c in password)
             {
                 if (notAllowedSymboolls.Contains(c))
                 {
-                    AcceptPass++;
+                    
+                    Console.WriteLine("Password must consist only of letters and digits");
+                    return false;
                 }
 
             }
-
-            if (AcceptPass == 0) { }
-            else
-            {
-                badPass = true;
-                Console.WriteLine("Password must consist only of letters and digits");
-            }
-
+            return true;
+        }
+        static bool HasEnoughtDigitAccept(string password)
+        {
+            string Nums = "1234567890";
             int numCount = 0;
             foreach (char c in password)
             {
@@ -45,17 +63,13 @@
                 }
 
             }
-            if (numCount >= 2) { }
+            if (numCount >= 2) return true;
             else
             {
-                badPass = true;
                 Console.WriteLine("Password must have at least 2 digits");
+                return false;
+                
             }
-
-            
-
-            if (badPass) { return; }
-            Console.WriteLine("Password is valid");
         }
     }
 }
