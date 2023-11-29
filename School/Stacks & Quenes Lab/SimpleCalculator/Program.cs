ce73a892fe20b13410b79452d0ee03354a961c64
andysharky <heckermanacc@gmail.com>
2023-11-29 13:26:21 +0200

    Quenes and stacks

---
+++ School/Stacks & Quenes Lab/SimpleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var values = input.Split(' ');
+
+            Stack<string> stack = new Stack<string>(values.Reverse());
+
+            while (stack.Count > 1)
+
+            {
+
+                int first = int.Parse(stack.Pop());
+
+                string oper = stack.Pop();
+
+                int second = int.Parse(stack.Pop());
+
+                switch (oper)
+                {
+                case "+":
+                    stack.Push((first + second).ToString());
+                        break;
+                case "-":
+                     stack.Push((first - second).ToString());
+                        break;
+                case "*":
+                     stack.Push((first * second).ToString());
+                     break;
+                case "/":
+                     stack.Push((first / second).ToString());
+                     break;
+
+                }
+
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
