e4c2ec51981fddca422bb28584c4ca4c04dda39d
andysharky <heckermanacc@gmail.com>
2023-12-06 11:56:29 +0200

    Exercises

---
+++ School/Lamda functions/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3 };
+            int[] num2 = num.Select(x => x / 2).ToArray();
+        }
+    }
+}
--- School/Stacks & Quenes Exercieses/FashionBotique/Program.cs
+++ School/Stacks & Quenes Exercieses/FashionBotique/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Stack<int> stack = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse) as Stack<int>;
+            string[] clothes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Stack<int> stack = new Stack<int>(clothes.Select(int.Parse));
             int capacity = int.Parse(Console.ReadLine());
             int racks = 0;
             int currentRack = 0;
@@ -14,8 +15,15 @@
                 {
                     currentRack += stack.Pop();
                 }
-                
+                else
+                {
+                    currentRack = stack.Pop();
+                    racks++;
+                }
+
             }
+            racks++;
+            Console.WriteLine(racks);
         }
     }
 }
