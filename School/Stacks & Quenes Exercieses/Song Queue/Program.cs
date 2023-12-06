80917af2bc5f90f45f775ffafe8404bc30db3bbd
andysharky <heckermanacc@gmail.com>
2023-12-06 13:27:47 +0200

    Exericse

--- School/Stacks & Quenes Exercieses/Song Queue/Program.cs
+++ School/Stacks & Quenes Exercieses/Song Queue/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] songsStr = Console.ReadLine().Split(", ");
+            Queue<string> queue = new Queue<string>(songsStr);
+            
+            while (queue.Count > 0)
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+                switch (command[0].ToLower())
+                {
+                    case "play":
+                        queue.Dequeue();
+                        break;
+                    case "add":
+                        command.RemoveAt(0);
+                        if (queue.Contains(string.Join(" ", command)))
+                        {
+                            Console.WriteLine($"{string.Join(" ", command)} is already contained!");
+                        }
+                        else
+                        {
+                            queue.Enqueue(string.Join(" ", command));
+                        }
+                        
+                        break;
+                    case "show":
+                        Console.WriteLine(string.Join(", ", queue));
+                        break;
+                }
+            }
+            Console.WriteLine("No more songs!");
         }
+        
     }
 }
