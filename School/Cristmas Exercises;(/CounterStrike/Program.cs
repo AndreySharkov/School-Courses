4f3a9d7b11120dffdd639de0a68af9ed44501da0
andysharky <heckermanacc@gmail.com>
2023-12-19 14:22:48 +0200

    Jerry ima pistolet

---
+++ School/Cristmas Exercises;(/CounterStrike/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string command;
+            int battlesWon = 0;
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                int commandInt = int.Parse(command);
+                
+                
+                if(energy < commandInt)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesWon} won battles and 0 energy");
+                    return;
+                }
+                energy -= commandInt;
+                battlesWon++;
+                if (battlesWon % 3 == 0)
+                {
+                    energy += battlesWon;
+                }
+
+
+            }
+            Console.WriteLine($"Won battles: {battlesWon}. Energy left: {energy}");
+
+        }
+    }
+}
