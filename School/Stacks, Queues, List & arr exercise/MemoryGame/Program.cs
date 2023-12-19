c1d835a1d8907689855bc549d97aadc8801a432e
andysharky <heckermanacc@gmail.com>
2023-12-19 11:16:13 +0200

    exercises

---
+++ School/Stacks, Queues, List & arr exercise/MemoryGame/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace MemoryGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nums = Console.ReadLine().Split().ToList();
+            string CommandArray = Console.ReadLine();
+            int moves = 1;
+            while (CommandArray != "end")
+            {
+                int[] commandArray = CommandArray.Split().Select(int.Parse).ToArray();
+                if (commandArray[1] == commandArray[0] ||  commandArray[0] < 0 || commandArray[1] < 0)
+                {
+                    Console.WriteLine("Invalid input! Adding additional elements to the board");
+                    nums.Insert(nums.Count / 2, "-" + moves + "a");
+                    nums.Insert(nums.Count / 2, "-" + moves + "a");
+                    
+                }
+                else if (nums[commandArray[0]] == nums[commandArray[1]])
+                {
+                    Console.WriteLine($"Congrats! You have found matching elements - {nums[commandArray[0]]}!");
+                    
+                    nums.RemoveAt(Math.Max(commandArray[1], commandArray[0]));
+                    nums.RemoveAt(Math.Min(commandArray[1], commandArray[0]));
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                }
+                moves++;
+                CommandArray = Console.ReadLine();
+
+            }
+            if (nums.Any())
+            {
+                Console.WriteLine("Sorry you lose :(");
+                Console.WriteLine(string.Join(" ", nums));
+            }
+            else
+            {
+                Console.WriteLine($"You have won in {moves} turns!");
+            }
+        }
+    }
+}
---
+++ School/Stacks, Queues, List & arr exercise/ThLift/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ThLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int[] lifts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for(int i = 0; i < lifts.Length; i++)
+            {
+                while(lifts[i] < 4 && people > 0)
+                {
+                    lifts[i]++;
+                    people--;
+                }
+            }
+            if(people > 0 && lifts[lifts.Length - 1] == 4)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+                Console.WriteLine(string.Join(" ", lifts));
+            }
+            else if(people >= 0 && lifts[lifts.Length -1] != 4)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", lifts));
+            }
+            else if (people == 0)
+            {
+                Console.WriteLine(string.Join(" ", lifts));
+            }
+        }
+    }
+}
