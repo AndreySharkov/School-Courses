90dd0be427c202f1c6bb719c9928577ef1117086
andysharky <heckermanacc@gmail.com>
2023-12-20 11:36:10 +0200

    jer

--- School/Cristmas Exercises;(/ShootForTheWin/Program.cs
+++ School/Cristmas Exercises;(/ShootForTheWin/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command;
+            int shotTargetsCount = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int index = int.Parse(command);
+
+                if (index < 0 || index >= targets.Length || targets[index] == -1)
+                {
+                    continue;
+                }
+
+                int shotValue = targets[index];
+                targets[index] = -1;
+                shotTargetsCount++;
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    if (targets[i] > shotValue)
+                    {
+                        targets[i] -= shotValue;
+                    }
+                    else if (targets[i] != -1)
+                    {
+                        targets[i] += shotValue;
+                    }
+                }
+            }
+            Console.WriteLine($"Shot targets: {shotTargetsCount} -> {string.Join(" ", targets)}");
         }
     }
 }
