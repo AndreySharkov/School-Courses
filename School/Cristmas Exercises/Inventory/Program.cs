10750f1579c6e1341f0a6b1736e1b2b8774b384a
andysharky <heckermanacc@gmail.com>
2023-12-23 09:48:11 +0200

    Cristmas exercises

--- School/Cristmas Exercises/Inventory/Program.cs
+++ School/Cristmas Exercises/Inventory/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> items = Console.ReadLine().Split(", ").ToList();
+            string commandString;
+            while((commandString = Console.ReadLine()) != "Craft!")
+            {
+                string[] command = commandString.Split("-").ToArray();
+                switch(command[0].Trim().ToLower())
+                {
+                    case "collect":
+                        items.Add(command[1].Trim());
+                        break;
+                    case "drop":
+                        items.Remove(command[1].Trim());
+                        break;
+                    case "combine items":
+                        if (items.Contains(command[1]))
+                        {
+                            List<string> Jerry = command[1].Split(":").ToList();
+                            items.Insert(items.IndexOf(Jerry[0]), Jerry[1]);
+                        }
+                        break;
+                    case "renew":
+                        items.Remove(command[1]);
+                        items.Add(command[1]);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", items));
         }
     }
 }
---
+++ School/Cristmas Exercises/Monster Extermination/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+class MonsterExtermination
+{
+    static void Main()
+    {
+        List<int> armorValues = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+        List<int> strikingImpactValues = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+
+        var result = BattleSimulation(armorValues, strikingImpactValues);
+
+        if (result.remainingStrikes.Count == 0)
+        {
+            Console.WriteLine("All monsters have been killed!");
+        }
+        else
+        {
+            Console.WriteLine("The soldier has been defeated.");
+        }
+
+        Console.WriteLine($"Total monsters killed: {result.killedMonsters}");
+    }
+
+    static (int killedMonsters, List<int> remainingStrikes) BattleSimulation(List<int> armorSequence, List<int> strikingImpactSequence)
+    {
+        int killedMonsters = 0;
+        List<int> remainingStrikes = new List<int>();
+
+        while (armorSequence.Any() && strikingImpactSequence.Any())
+        {
+            int currentArmor = armorSequence.First();
+            int currentImpact = strikingImpactSequence.Last();
+
+            if (currentImpact >= currentArmor)
+            {
+                killedMonsters++;
+
+                if (strikingImpactSequence.Count > 1)
+                {
+                    strikingImpactSequence[strikingImpactSequence.Count - 2] += currentImpact - currentArmor;
+                }
+            }
+            else
+            {
+                armorSequence[0] -= currentImpact;
+                remainingStrikes.Insert(0, strikingImpactSequence.Last());
+            }
+
+            armorSequence.RemoveAt(0);
+            strikingImpactSequence.RemoveAt(strikingImpactSequence.Count - 1);
+        }
+
+        return (killedMonsters, remainingStrikes);
+    }
+}
