0ed70c557fd9b3849f844dcdfb3ee2c2980198ec
andysharky <heckermanacc@gmail.com>
2024-01-05 20:01:46 +0200

    2D jeri

--- School/Multidimensional Arrays/Jagged-Array Modification/Program.cs
+++ School/Multidimensional Arrays/Jagged-Array Modification/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+            string commandString;
+            while ((commandString = Console.ReadLine()) != "END")
+            {
+                string[] command = commandString.Split().ToArray();
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+                
+                if (IsValidCoordinates(row, col, n))
+                {
+                    switch (command[0].ToLower())
+                    {
+                        case "add":
+
+                            matrix[row][col] += value;
+                            break;
+                        case "subtract":
+                            matrix[row][col] -= value;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+            }
+            foreach (int[] row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
         }
+        
+        static bool IsValidCoordinates(int row, int col, int n)
+        {
+            return row >= 0 && row < n && col >= 0 && col < n;
+        }
+
     }
 }
