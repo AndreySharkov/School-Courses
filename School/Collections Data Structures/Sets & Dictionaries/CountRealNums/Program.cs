6bffe03e7fea68c6fce0a1f328ac70374ceeaa98
andysharky <heckermanacc@gmail.com>
2024-02-07 17:55:37 +0200

    school

---
+++ School/Collections Data Structures/Sets & Dictionaries/CountRealNums/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountRealNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] doubles = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            var counts = new SortedDictionary<int, double>();
+            foreach (int i in doubles)
+            {
+                if(counts.ContainsKey(i))
+                {
+                    counts[i]++;
+                }
+                else
+                {
+                    counts.Add(i, 1);
+                }
+            }
+            foreach (var i in counts)
+            {
+                Console.WriteLine($"{i.Key} -> {i.Value}");
+            }
+        }
+    }
+}
---
+++ School/Collections Data Structures/Sets & Dictionaries/Odd Occurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            Dictionary<string, int> wordCount = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            foreach (string word in words)
+            {
+                string lowercaseWord = word.ToLower();
+                if (wordCount.ContainsKey(lowercaseWord))
+                {
+                    wordCount[lowercaseWord]++;
+                }
+                else
+                {
+                    wordCount[lowercaseWord] = 1;
+                }
+            }
+            foreach (var entry in wordCount)
+            {
+                if (entry.Value % 2 != 0)
+                {
+                    Console.Write(entry.Key + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
