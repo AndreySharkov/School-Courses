25d92c25d9ad16ab59f35d7faa08ec32851db0e2
andysharky <heckermanacc@gmail.com>
2024-02-09 11:56:10 +0200

    jeri

--- School/Collections Data Structures/Sets & Dictionaries/WordSynonyms/Program.cs
+++ School/Collections Data Structures/Sets & Dictionaries/WordSynonyms/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> synonymsDictionary = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonymsDictionary.ContainsKey(word))
+                {
+                    synonymsDictionary[word] = new List<string>();
+                }
+
+                synonymsDictionary[word].Add(synonym);
+            }
+
+            foreach (var kvp in synonymsDictionary)
+            {
+                string word = kvp.Key;
+                List<string> synonyms = kvp.Value;
+
+                Console.WriteLine($"{word} - {string.Join(", ", synonyms)}");
+            }
         }
     }
 }
