b249723f11c618870ff84ff6cf59b158ffc1e272
andysharky <heckermanacc@gmail.com>
2025-01-28 08:35:30 +0200

    SoftUniTest

---
+++ C#/Classes and Objects/FinalExam1/Competitor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Competitor<T> : IEnumerable<T>, IComparable<Competitor<T>>
+{
+    private string name;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        private set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        private set
+        {
+            if (value < 10)
+            {
+                throw new ArgumentOutOfRangeException("Age cannot be less than 10.");
+            }
+            age = value;
+        }
+    }
+
+    public List<T> Scores { get; private set; }
+
+    public Competitor(string name, int age)
+    {
+        Name = name;
+        Age = age;
+        Scores = new List<T>();
+    }
+
+    public void Add(T score)
+    {
+        Scores.Add(score);
+    }
+
+    public int CountCompetitions()
+    {
+        return Scores.Count;
+    }
+
+    public T ChangeLastScore(T newScore)
+    {
+        if (Scores.Count == 0)
+        {
+            throw new InvalidOperationException("No scores to change.");
+        }
+
+        T oldScore = Scores[Scores.Count - 1];
+        Scores[Scores.Count - 1] = newScore;
+        return oldScore;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return Scores.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int CompareTo(Competitor<T> other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+        int nameComparison = string.Compare(this.Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        if (nameComparison == 0)
+        {
+            return nameComparison;
+        }
+
+        return this.Age.CompareTo(other.Age);
+    }
+}
---
+++ C#/Classes and Objects/FinalExam1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Competitor<int> IvanTheWiner = new Competitor<int>("Ivan", 16);
+        Competitor<int> Vanya = new Competitor<int>("Vanya", 17);
+        IvanTheWiner.Add(6);
+        IvanTheWiner.Add(2);
+        IvanTheWiner.Add(5);
+        IvanTheWiner.Add(4);
+
+        Console.WriteLine(IvanTheWiner.CompareTo(Vanya));
+    }
+}
+
